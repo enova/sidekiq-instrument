a2442bff6c7a5631b7b09088ddf3157ea1089a3a
orioldsm <150839949+orioldsm@users.noreply.github.com>
2024-08-15 15:03:38 -0500

    modify client to filter out extra enqueue metrics
    Signed-off-by: orioldsm <150839949+orioldsm@users.noreply.github.com>

--- lib/sidekiq/instrument/middleware/client.rb
+++ lib/sidekiq/instrument/middleware/client.rb
@@ -11,13 +11,20 @@ module Sidekiq::Instrument
       # worker_class is a const in sidekiq >= 6.x
       klass = Object.const_get(worker_class.to_s)
       class_instance = klass.new
-      # Commenting out the StatsD line just to remove other metrics using enqueue verbiage
-      # Statter.statsd.increment(metric_name(class_instance, 'enqueue'))
-      Statter.dogstatsd&.increment('sidekiq.added_to_queue', worker_dog_options(class_instance))
+
+      # This is needed because the ClientMiddleware is called twice for scheduled jobs
+      # - Once when it gets scheduled
+      # - Once when it gets dequeued for processing
+      # We only want to increment the enqueue metric when the job is scheduled and
+      # Sidekiq::Context.current[:class] is only ever set when the job is scheduled
+      if Sidekiq::Context.current[:class].present?
+        Statter.statsd.increment(metric_name(class_instance, 'enqueue'))
+        Statter.dogstatsd&.increment('sidekiq.enqueue', worker_dog_options(class_instance))
+        Statter.dogstatsd&.flush(sync: true)
+      end
+
       WorkerMetrics.trace_workers_increment_counter(klass.name.underscore, redis_pool)
-      result = yield
-      Statter.dogstatsd&.flush(sync: true)
-      result
+      yield
     end
   end
 end

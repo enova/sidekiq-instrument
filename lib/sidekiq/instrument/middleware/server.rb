d892929959488f79dea7222dda44fbfd628c9864
orioldsm <150839949+orioldsm@users.noreply.github.com>
2024-08-16 14:06:10 -0500

    klass -> worker
    Signed-off-by: orioldsm <150839949+orioldsm@users.noreply.github.com>

--- lib/sidekiq/instrument/middleware/server.rb
+++ lib/sidekiq/instrument/middleware/server.rb
@@ -20,7 +20,7 @@ module Sidekiq::Instrument
     rescue StandardError => e
       # if we have retries left, increment the enqueue.retry counter to indicate the job is going back on the queue
       if max_retries(worker) > current_retries(job)
-        WorkerMetrics.trace_workers_increment_counter(klass.name.underscore, redis_pool)
+        WorkerMetrics.trace_workers_increment_counter(worker.name.underscore, redis_pool)
         Statter.dogstatsd&.increment('sidekiq.enqueue.retry', worker_dog_options(worker))
       end
 

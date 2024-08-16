72814669815141fea9022b1768e1eabced3cb536
orioldsm <150839949+orioldsm@users.noreply.github.com>
2024-08-16 13:50:05 -0500

    add retry metrics
    Signed-off-by: orioldsm <150839949+orioldsm@users.noreply.github.com>

--- lib/sidekiq/instrument/middleware/server.rb
+++ lib/sidekiq/instrument/middleware/server.rb
@@ -7,9 +7,10 @@ module Sidekiq::Instrument
   class ServerMiddleware
     include Sidekiq::Instrument::MetricNames
 
-    def call(worker, _job, _queue, &block)
-      Statter.statsd.increment(metric_name(worker, 'dequeue'))
-      Statter.dogstatsd&.increment('sidekiq.dequeue', worker_dog_options(worker))
+    def call(worker, job, _queue, &block)
+      dequeue_string = is_retry(job) ? 'dequeue.retry' : 'dequeue'
+      Statter.dogstatsd&.increment(dequeue_string, worker_dog_options(worker))
+      Statter.statsd.increment(metric_name(worker, dequeue_string))
 
       start_time = Time.now
       yield block
@@ -17,6 +18,12 @@ module Sidekiq::Instrument
       Statter.statsd.measure(metric_name(worker, 'runtime'), execution_time_ms)
       Statter.dogstatsd&.timing('sidekiq.runtime', execution_time_ms, worker_dog_options(worker))
     rescue StandardError => e
+      # if we have retries left, increment the enqueue.retry counter to indicate the job is going back on the queue
+      if max_retries(worker) > current_retries(job)
+        WorkerMetrics.trace_workers_increment_counter(klass.name.underscore, redis_pool)
+        Statter.dogstatsd&.increment('sidekiq.enqueue.retry', worker_dog_options(worker)) unless current_retries(job) == max_retries(worker) # dont increment for last retry
+      end
+
       Statter.statsd.increment(metric_name(worker, 'error'))
       Statter.dogstatsd&.increment('sidekiq.error', worker_dog_options(worker))
       raise e
@@ -24,5 +31,15 @@ module Sidekiq::Instrument
       WorkerMetrics.trace_workers_decrement_counter(worker.class.to_s.underscore)
       Statter.dogstatsd&.flush(sync: true)
     end
+  end
+
+  private
+
+  def current_retries(job)
+    job["redis_throttler_params"]["retry_count"]
+  end
+
+  def is_retry(job)
+    current_retries(job) > 0
   end
 end

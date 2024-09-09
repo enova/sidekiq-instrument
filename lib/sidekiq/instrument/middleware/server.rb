b76e5b4bd1d49c91d4bf998cc4800cf958d25a64
orioldsm <150839949+orioldsm@users.noreply.github.com>
2024-09-09 13:17:17 -0500

    rescue all exceptions for metrics (#32)

--- lib/sidekiq/instrument/middleware/server.rb
+++ lib/sidekiq/instrument/middleware/server.rb
@@ -15,17 +15,21 @@ module Sidekiq::Instrument
       start_time = Time.now
       yield block
       execution_time_ms = (Time.now - start_time) * 1000
-      Statter.statsd.measure(metric_name(worker, 'runtime'), execution_time_ms)
       Statter.dogstatsd&.timing('sidekiq.runtime', execution_time_ms, worker_dog_options(worker))
-    rescue StandardError => e
+      Statter.statsd.measure(metric_name(worker, 'runtime'), execution_time_ms)
+    rescue Exception => e
+      dd_options = worker_dog_options(worker)
+      dd_options[:tags] << "error:#{e.class.name}"
+
       # if we have retries left, increment the enqueue.retry counter to indicate the job is going back on the queue
       if max_retries(worker) > current_retries(job) + 1
         WorkerMetrics.trace_workers_increment_counter(worker.class.to_s.underscore)
-        Statter.dogstatsd&.increment('sidekiq.enqueue.retry', worker_dog_options(worker))
+        Statter.dogstatsd&.increment('sidekiq.enqueue.retry', dd_options)
       end
 
+      Statter.dogstatsd&.increment('sidekiq.error', dd_options)
       Statter.statsd.increment(metric_name(worker, 'error'))
-      Statter.dogstatsd&.increment('sidekiq.error', worker_dog_options(worker))
+
       raise e
     ensure
       WorkerMetrics.trace_workers_decrement_counter(worker.class.to_s.underscore)
